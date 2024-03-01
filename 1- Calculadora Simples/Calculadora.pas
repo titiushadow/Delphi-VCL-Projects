15a990268b0dd15fb8403521834c9009240b88ee
Mesaque <63453751+titiushadow@users.noreply.github.com>
2024-03-01 15:53:46 -0300

    Alterando nome das variaveis

--- 1- Calculadora Simples/Calculadora.pas
+++ 1- Calculadora Simples/Calculadora.pas
@@ -60,11 +60,11 @@ end;
 // Multiplicação
 procedure TCalculadora_simples.Click_multiplicacao(Sender: TObject);
 var
-  num1, num2, resultado: Double;
+  numero1, num2, resultado: Double;
 begin
-  num1 := StrToFloat(n1.Text);
+  numero1 := StrToFloat(n1.Text);
   num2 := StrToFloat(n2.Text);
-  resultado := num1 * num2;
+  resultado := numero1 * num2;
   lbl_resultado.Caption := Format('Resulado: %.2F', [resultado]);
 end;
 
@@ -72,15 +72,15 @@ end;
 // Divisão
 procedure TCalculadora_simples.Click_divisao(Sender: TObject);
 var
-  num1, num2, resultado: Double;
+  numero1, numero2, resultado: Double;
 begin
-  num1 := StrToFloat(n1.Text);
-  num2 := StrToFloat(n2.Text);
+  numero1 := StrToFloat(n1.Text);
+  numero2 := StrToFloat(n2.Text);
 
   resultado := 0.0;
 
-  if num2 <> 0 then
-    resultado := num1 / num2
+  if numero2 <> 0 then
+    resultado := numero1 / numero2
   else
   begin
     ShowMessage('Não é possivel dividir por 0');
